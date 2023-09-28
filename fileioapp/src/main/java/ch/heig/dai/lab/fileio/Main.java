a83d7dc312686d86dd7be046a0c678c570f4e304
RemiAncay <remi.ancay@heig-vd.ch>
2023-09-28 11:03:33 +0200

    Create folders for RemiAncay

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
@@ -2,8 +2,7 @@ package ch.heig.dai.lab.fileio;
 
 import java.io.File;
 
-// *** TODO: Change this to import your own package ***
-import ch.heig.dai.lab.fileio.jehrensb.*;
+import ch.heig.dai.lab.fileio.RemiAncay.*;
 
 public class Main {
     // *** TODO: Change this to your own name ***
