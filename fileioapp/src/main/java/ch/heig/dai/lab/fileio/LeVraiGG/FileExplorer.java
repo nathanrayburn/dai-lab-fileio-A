9c684c503d1cc607451d4a0645a9608c5d074464
LeVraiGG <tux.caroline@gmail.com>
2023-10-05 11:46:19 +0200

    Remove imports unused

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/LeVraiGG/FileExplorer.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/LeVraiGG/FileExplorer.java
@@ -1,9 +1,7 @@
 package ch.heig.dai.lab.fileio.LeVraiGG;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Vector;
 
 public class FileExplorer {
     private final File folder;
