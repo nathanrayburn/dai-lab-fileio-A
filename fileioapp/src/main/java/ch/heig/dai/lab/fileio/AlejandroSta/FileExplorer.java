ba080bd1755947cff37fc9f4778bddbe5b006484
AlejandroSta <alejandro.stadlin@heig-vd.ch>
2023-10-05 11:38:48 +0200

    Implement FileExplorer

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/AlejandroSta/FileExplorer.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/AlejandroSta/FileExplorer.java
@@ -26,6 +26,20 @@ public class FileExplorer {
      */
     public File getNewFile() {
         // TODO: implement the method body here
+        File[] liste = folder.listFiles();
+        for(File f1 : liste){
+            boolean already_known = false;
+            for(File f2 : knownFiles){
+                if(f1.equals(f2)){
+                    already_known = true;
+                    break;
+                }
+            }
+            if(!already_known){
+                knownFiles.add(f1);
+                return f1;
+            }
+        }
         return null;
     }
 }
