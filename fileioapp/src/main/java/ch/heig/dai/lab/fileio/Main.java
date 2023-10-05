9ea265d1023b836a381182bb03ce6c2e66ea8930
andcost <toscmen.com@gmail.com>
2023-10-05 10:58:53 +0200

    reverted main change fix

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
@@ -11,16 +11,12 @@ public class Main {
 
     /**
      * Main method to transform files in a folder.
-     * Create the necessary objects (FileExplorer, EncodingSelector,
-     * FileReaderWriter, Transformer).
-     * In an infinite loop, get a new file from the FileExplorer, determine its
-     * encoding with the EncodingSelector,
-     * read the file with the FileReaderWriter, transform the content with the
-     * Transformer, write the result with the
+     * Create the necessary objects (FileExplorer, EncodingSelector, FileReaderWriter, Transformer).
+     * In an infinite loop, get a new file from the FileExplorer, determine its encoding with the EncodingSelector,
+     * read the file with the FileReaderWriter, transform the content with the Transformer, write the result with the
      * FileReaderWriter.
      * 
-     * Result files are written in the same folder as the input files, and encoded
-     * with UTF8.
+     * Result files are written in the same folder as the input files, and encoded with UTF8.
      *
      * File name of the result file:
      * an input file "myfile.utf16le" will be written as "myfile.utf16le.processed",
@@ -29,8 +25,7 @@ public class Main {
     public static void main(String[] args) {
         // Read command line arguments
         if (args.length != 2 || !new File(args[0]).isDirectory()) {
-            System.out.println(
-                    "You need to provide two command line arguments: an existing folder and the number of words per line.");
+            System.out.println("You need to provide two command line arguments: an existing folder and the number of words per line.");
             System.exit(1);
         }
         String folder = args[0];
