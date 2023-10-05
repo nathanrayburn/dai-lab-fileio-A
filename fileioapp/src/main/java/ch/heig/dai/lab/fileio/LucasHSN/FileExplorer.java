af5284c301f709bf75746f7a41e3c6759e7a13d4
LucasHSN <lucas.hussain@heig-vd.ch>
2023-10-05 11:22:00 +0200

    Create file explorer feature

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/LucasHSN/FileExplorer.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/LucasHSN/FileExplorer.java
@@ -25,7 +25,17 @@ public class FileExplorer {
      * @return a new file, or null if there is no new file
      */
     public File getNewFile() {
-        // TODO: implement the method body here
+        File[] filesInFolder = folder.listFiles();
+
+        if (filesInFolder != null) {
+            for (File file : filesInFolder) {
+                // Check if the file not in the knownFiles HashSet
+                if (!knownFiles.contains(file)) {
+                    knownFiles.add(file);
+                    return file; // Return the new file
+                }
+            }
+        }
         return null;
     }
 }
