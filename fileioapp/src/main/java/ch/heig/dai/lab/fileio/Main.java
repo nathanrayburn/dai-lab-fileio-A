bbd5a952b2c15898f83bf4dfa5b26c60dcb6e9b3
Sinaf19 <quentinsurdez@gmail.com>
2023-10-05 10:59:00 +0200

    Create folders for Sinaf19

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
@@ -3,7 +3,7 @@ package ch.heig.dai.lab.fileio;
 import java.io.File;
 
 // *** TODO: Change this to import your own package ***
-import ch.heig.dai.lab.fileio.jehrensb.*;
+import ch.heig.dai.lab.fileio.Sinaf19.*;
 
 public class Main {
     // *** TODO: Change this to your own name ***
