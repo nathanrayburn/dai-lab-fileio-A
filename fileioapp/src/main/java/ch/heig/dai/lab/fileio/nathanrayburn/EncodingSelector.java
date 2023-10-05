1cad1264b9df61ad2999ca72e2337a1e29d5425d
Nathan Rayburn <nathanrayburn2001@gmail.com>
2023-10-05 21:06:28 +0200

    implementation file encoding

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/nathanrayburn/EncodingSelector.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/nathanrayburn/EncodingSelector.java
@@ -17,7 +17,26 @@ public class EncodingSelector {
      * @return the encoding of the file, or null if the extension is not recognized
      */
     public Charset getEncoding(File file) {
-        // TODO: implement the method body here
-        return null;
+
+        String fileName = file.getName();
+
+        String fileExt  = "";
+
+        int index = fileName.lastIndexOf('.');
+
+        if(index > 0) fileExt = fileName.substring(index);
+
+        switch(fileExt) {
+            case ".utf8"    :
+                return Charset.forName("UTF-8");
+            case ".txt"     :
+                return Charset.forName("US-ASCII");
+            case ".utf16be" :
+                return Charset.forName("UTF-16BE");
+            case ".utf16le" :
+                return Charset.forName("UTF-16LE");
+            default :
+                return null;
+        }
     }
 }
