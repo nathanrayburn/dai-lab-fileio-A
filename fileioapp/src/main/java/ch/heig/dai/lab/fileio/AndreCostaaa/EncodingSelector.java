5512396f74212a904719c30ec29e79760184fcba
andcost <toscmen.com@gmail.com>
2023-10-05 11:11:32 +0200

    implemented encoding selector

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/AndreCostaaa/EncodingSelector.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/AndreCostaaa/EncodingSelector.java
@@ -2,8 +2,36 @@ package ch.heig.dai.lab.fileio.AndreCostaaa;
 
 import java.io.File;
 import java.nio.charset.Charset;
+import java.util.HashMap;
 
 public class EncodingSelector {
+
+    private final HashMap<String, Charset> extensionToCharsetMap = new HashMap<String, Charset>() {
+        {
+            put(".utf8", Charset.forName("UTF-8"));
+            put(".txt", Charset.forName("US-ASCII"));
+            put(".utf16be", Charset.forName("UTF-16BE"));
+            put(".utf16le", Charset.forName("UTF-16LE"));
+        }
+    };
+
+    private final char EXTENSION_SEPARATOR = '.';
+
+    /**
+     * Gets the file extension of a file
+     * 
+     * @param fileName
+     * @return the file extension or null if the file has no extension
+     */
+    private String getFileExtension(File file) {
+
+        final String fileName = file.getName();
+
+        if (fileName.indexOf(EXTENSION_SEPARATOR) == -1)
+            return null;
+
+        return fileName.substring(fileName.lastIndexOf(EXTENSION_SEPARATOR));
+    }
 
     /**
      * Get the encoding of a file based on its extension.
@@ -17,7 +45,11 @@ public class EncodingSelector {
      * @return the encoding of the file, or null if the extension is not recognized
      */
     public Charset getEncoding(File file) {
-        // TODO: implement the method body here
-        return null;
+
+        final String fileExtension = getFileExtension(file);
+        if (fileExtension == null) {
+            return null;
+        }
+        return extensionToCharsetMap.get(fileExtension);
     }
 }
