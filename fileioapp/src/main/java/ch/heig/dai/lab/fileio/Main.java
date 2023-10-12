f5771675198ee73397a6773d2ba1488546179e76
nathan <nathanarmand@hotmail.com>
2023-10-12 11:46:33 +0200

    finished main

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
@@ -3,11 +3,11 @@ package ch.heig.dai.lab.fileio;
 import java.io.File;
 
 // *** TODO: Change this to import your own package ***
-import ch.heig.dai.lab.fileio.jehrensb.*;
+import ch.heig.dai.lab.fileio.nathanrayburn.*;
 
 public class Main {
     // *** TODO: Change this to your own name ***
-    private static final String newName = "Jean-Claude Van Damme";
+    private static final String newName = "nathanrayburn";
 
     /**
      * Main method to transform files in a folder.
@@ -15,7 +15,7 @@ public class Main {
      * In an infinite loop, get a new file from the FileExplorer, determine its encoding with the EncodingSelector,
      * read the file with the FileReaderWriter, transform the content with the Transformer, write the result with the
      * FileReaderWriter.
-     * 
+     *
      * Result files are written in the same folder as the input files, and encoded with UTF8.
      *
      * File name of the result file:
@@ -32,11 +32,44 @@ public class Main {
         int wordsPerLine = Integer.parseInt(args[1]);
         System.out.println("Application started, reading folder " + folder + "...");
         // TODO: implement the main method here
-
+        FileExplorer dir = new FileExplorer(folder);
+        EncodingSelector encoding = new EncodingSelector();
+        FileReaderWriter fileRW = new FileReaderWriter();
+        Transformer transformer = new Transformer(newName, wordsPerLine);
         while (true) {
             try {
                 // TODO: loop over all files
+               File file =  dir.getNewFile();
+               if(file==null) {
+                   System.out.println("No more files to process\n");
+                   break;
+               }
 
+               if(encoding.getEncoding(file) == null) {
+                   System.out.println("Unsupported file format for file : " + file.getName());
+                   continue;
+               }
+
+               String content = fileRW.readFile(file,encoding.getEncoding(file));
+
+               if (content == null) {
+                    System.out.println("Error reading file: " + file.getName());
+                    continue;
+                }
+
+               String convertedContent = transformer.replaceChuck(content);
+
+               convertedContent = transformer.capitalizeWords(convertedContent);
+               convertedContent = transformer.wrapAndNumberLines(convertedContent);
+
+               File output = new File(file.getAbsolutePath() + ".processed");
+
+                boolean isFileWritten = fileRW.writeFile(output,convertedContent,encoding.getEncoding(file));
+                if (isFileWritten) {
+                    System.out.println("Processed and wrote to: " + output.getName());
+                } else {
+                    System.out.println("Failed to write to: " + output.getName());
+                }
             } catch (Exception e) {
                 System.out.println("Exception: " + e);
             }
