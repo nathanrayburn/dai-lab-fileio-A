7fac10bb0f1559419a874af65b734b19ea685a49
michaelstrefeler <mstrefeler@me.com>
2023-10-12 11:19:32 +0200

    Implemented FileReaderWriter for michaelstrefeler

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/michaelstrefeler/FileReaderWriter.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/michaelstrefeler/FileReaderWriter.java
@@ -11,11 +11,16 @@ public class FileReaderWriter {
      * @param encoding
      * @return the content of the file as a String, or null if an error occurred.
      */
-    public String readFile(File file, Charset encoding) {
-        // TODO: Implement the method body here. 
-        // Use the ...Stream and ...Reader classes from the java.io package.
-        // Make sure to close the streams and readers at the end.
-        return null;
+    public String readFile(File file, Charset encoding){
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), encoding))){
+            StringBuilder contents = new StringBuilder();
+            int c; // is -1 if end of file or char value 0-65535
+            while ((c = reader.read()) != -1) contents.append(c);
+            reader.close();
+            return contents.toString();
+        }catch (IOException e) {
+            return null;
+        }
     }
 
     /**
@@ -26,9 +31,13 @@ public class FileReaderWriter {
      * @return true if the file was written successfully, false otherwise
      */
     public boolean writeFile(File file, String content, Charset encoding) {
-        // TODO: Implement the method body here. 
-        // Use the ...Stream and ...Reader classes from the java.io package.
-        // Make sure to flush the data and close the streams and readers at the end.
-        return false;
+        try(BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), encoding))){
+            writer.write(content, 0, content.length());
+            writer.flush();
+            writer.close();
+            return readFile(file, encoding).isEmpty();
+        }catch (IOException e) {
+            return false;
+        }
     }
 }
